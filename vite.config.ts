1b2743e0247867123cb6b166f2029e9071d69895
danjam <d.s.james@gmail.com>
2025-08-03 18:50:07 +0100

    Updates for TS

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  server: {
-    port: 3000,
-    open: true,
-  },
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' }
+  }
 })
