4ea640ee2fc04c215fc886ca1c30ae08822da1f3
danjam <d.s.james@gmail.com>
2025-08-17 04:05:58 +0100

    Fixed import

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 // src/main.tsx
 import { StrictMode } from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 
 import App from '@/App.tsx';
 import '@/index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>
