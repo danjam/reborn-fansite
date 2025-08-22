36904414d19e8e11370e4e5a377b05df143595cd
danjam <d.s.james@gmail.com>
2025-08-22 12:35:05 +0100

    feat: implemented analytics

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_REACT_APP_VERSION: string;
   readonly VITE_REACT_APP_GIT_HASH?: string;
+  readonly VITE_GA_MEASUREMENT_ID?: string;
 }
 
 interface ImportMeta {
